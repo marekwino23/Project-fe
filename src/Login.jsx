497c90030e3959b4566bb447e07aca18188409cb
marekwino23 <marekw1996@gmail.com>
2021-02-28 20:03:06 +0100

    fix warnings

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useReducer, useState } from 'react';
 import cogoToast from 'cogo-toast';
 import {Link, useHistory } from 'react-router-dom';
-import Forgotpassword  from './components/ForgotPassword';
-import obrazek from "./logo.svg";
-import { getURL } from './helpers';
-
 
 const initial = { loading: false, error: '' };
 
